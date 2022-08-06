2fe091ccfcaa0e3bc768c45e1d5bd3c1ea49d736
Almádi Gergely <109812844+gregalmadi@users.noreply.github.com>
2022-08-06 18:06:09 +0200

    Add files via upload

---
+++ js1-projekt-modal-gregalmadi/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// HTML selectors
+const container = document.querySelector(".container");
+const content = document.querySelectorAll(".content");
+const contentText = document.querySelectorAll(".content__text");
+const header = document.querySelectorAll(".content__header");
+const modalWindow = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal__content");
+const modalHeader = document.querySelector(".modal__header");
+const closeModalBtn = document.querySelector(".btn--close");
+
+// Opening modal logic
+const modalOpen = (i) => {
+  modalWindow.style.display = "flex";
+  modalWindow.classList.add("fadeIn");
+  setTimeout(() => {
+    modalWindow.classList.remove("fadeIn");
+  }, 1000);
+  modalContent.innerHTML = contentText[i].innerHTML;
+  modalHeader.innerHTML = header[i].innerHTML;
+};
+
+// Closing modal logic
+const modalClose = () => {
+  modalWindow.classList.add("fadeOut");
+
+  setTimeout(() => {
+    modalWindow.classList.remove("fadeOut");
+    modalWindow.style.display = "none";
+  }, 1000);
+};
+
+// Listening to click events on cards
+content.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    modalOpen(i);
+  });
+});
+
+// Listening for modal closing events
+closeModalBtn.addEventListener("click", modalClose);
+//modalWindow.addEventListener("click", modalClose);
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    modalClose();
+  }
+});
