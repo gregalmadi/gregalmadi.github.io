08035186af3eed17c73487482d65d60024b38630
Almádi Gergely <109812844+gregalmadi@users.noreply.github.com>
2022-08-08 14:11:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 "use strict";
 
-const cards = document.querySelectorAll(".card");
-const content = document.querySelectorAll(".content");
-
-/*content.forEach((card, i) =>
-  card.addEventListener("click", () => {
-    console.log("card clicked");
-    card.classList.add("flipCard");
-  })
-);*/
