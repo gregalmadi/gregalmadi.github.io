58b51ed4aab2664bf1b635f1f903e991af888c68
Almádi Gergely <109812844+gregalmadi@users.noreply.github.com>
2022-09-12 09:21:14 +0200

    Add files via upload

--- js-got/js/got.js
+++ js-got/js/got.js
@@ -13,15 +13,21 @@ const characterBio = document.querySelector(".bio__character");
 const searchValue = document.querySelector(".input");
 const buttonSearch = document.querySelector(".search");
 const searchInfo = document.querySelector(".notFound");
+const sidebarOpeningButton = document.querySelector(".btn__sidebar--open");
+const gotHeader = document.querySelector(".header");
+const sidebar = document.querySelector(".section__sidebar");
 
 let tileImages;
+let isOpen = true;
 
 //--------------------------------------------------
 // Sorting character data and filtering alive characters
 const aliveAndSortedCharData = charData
   .sort((a, b) => {
-    let x = a.portrait.slice(7).slice(0, -4);
-    let y = b.portrait.slice(7).slice(0, -4);
+    let x = a.name.split(" ")[0];
+    let y = b.name.split(" ")[0];
+    //let x = a.portrait.slice(7).slice(0, -4);
+    //let y = b.portrait.slice(7).slice(0, -4);
     return x < y ? -1 : x > y ? 1 : 0;
   })
   .filter((char) => !char?.dead);
@@ -45,9 +51,13 @@ const aliveAndSortedCharData = charData
   tileImages.forEach((img, i) => {
     img.setAttribute("src", `${aliveAndSortedCharData[i].portrait}`);
   });
-  tileNames.forEach(
-    (charName, i) => (charName.innerHTML = aliveAndSortedCharData[i].name)
-  );
+  tileNames.forEach((charName, i) => {
+    if (aliveAndSortedCharData[i].alias) {
+      charName.innerHTML = aliveAndSortedCharData[i].alias;
+    } else {
+      charName.innerHTML = aliveAndSortedCharData[i].name;
+    }
+  });
 })();
 
 //------------------------------------
@@ -62,7 +72,7 @@ const showDetails = (img, i) => {
   if (aliveAndSortedCharData[i]?.picture.includes("jpg")) {
     characterImage.setAttribute("src", `${aliveAndSortedCharData[i].picture}`);
   } else {
-    characterImage.setAttribute("src", `assets/pictures/noPic.jpg`);
+    characterImage.setAttribute("src", `./assets/pictures/noPic.jpg`);
   }
 
   characterName.innerHTML = `${aliveAndSortedCharData[i].name}`;
@@ -70,15 +80,15 @@ const showDetails = (img, i) => {
   if (aliveAndSortedCharData[i]?.house)
     characterBadge.setAttribute(
       "src",
-      `../assets/houses/${aliveAndSortedCharData[i].house}.png`
+      `./assets/houses/${aliveAndSortedCharData[i].house}.png`
     );
   else if (aliveAndSortedCharData[i]?.organization) {
     characterBadge.setAttribute(
       "src",
-      `../assets/houses/${aliveAndSortedCharData[i].organization}.png`
+      `./assets/houses/${aliveAndSortedCharData[i].organization}.png`
     );
   } else {
-    characterBadge.setAttribute("src", `../assets/houses/noHouse.png`);
+    characterBadge.setAttribute("src", `./assets/houses/noHouse.png`);
   }
 
   characterBio.innerHTML = `${aliveAndSortedCharData[i].bio}`;
@@ -89,6 +99,7 @@ const showDetails = (img, i) => {
 tileImages.forEach((img, i) => {
   img.addEventListener("click", () => {
     showDetails(img, i);
+    openSidebar();
   });
 });
 
@@ -102,10 +113,77 @@ buttonSearch.addEventListener("click", () => {
       char.name.toLowerCase().includes(searchedCharacter) &&
       searchedCharacter !== ""
     ) {
+      openSidebar();
       showDetails(tileImages[i], i);
+      searchValue.value = "";
       break;
     } else {
       searchInfo.innerHTML = "No character found";
+      searchValue.value = "";
     }
   }
 });
+
+// Sidebar opening / closing functionality
+sidebarOpeningButton.addEventListener("click", () => {
+  if (isOpen) {
+    closeSidebar();
+  } else {
+    openSidebar();
+  }
+});
+
+const openSidebar = () => {
+  gotHeader.innerHTML = "Game Of Thrones";
+  sidebar.style.width = "400px";
+  buttonSearch.style.visibility = "visible";
+  searchValue.style.visibility = "visible";
+  characterName.style.display = "block";
+  characterImage.style.display = "block";
+  characterBadge.style.display = "block";
+  characterBio.style.display = "block";
+  isOpen = true;
+};
+
+const closeSidebar = () => {
+  gotHeader.innerHTML = "GOT";
+  sidebar.style.width = "100px";
+  buttonSearch.style.visibility = "hidden";
+  searchValue.style.visibility = "hidden";
+  characterName.style.display = "none";
+  characterImage.style.display = "none";
+  characterBadge.style.display = "none";
+  characterBio.style.display = "none";
+  isOpen = false;
+};
+
+//----------------------------------------------------------------------------------
+//alternative solution for fetching json data (as if it was coming from a webserver)
+/*
+const fetchUrl = "../json/got.json";
+
+const fetchingData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`HTTP Error: ${response.status}.`);
+
+    const data = await response.json();
+  } catch (err) {
+    throw new Error("Something went wrong with your fetching request!");
+  }
+  // filtering and sorting from this point is the same
+};
+
+fetchingData(fetchUrl);
+
+//----------------------------------------------------------------------------------
+//alternative solution for sorting based on surname instead of firstname
+
+const aliveAndSortedCharData = charData
+  .sort((a, b) => {
+    let x = a.name?.split(" ")[1].trim();
+    let y = b.name?.split(" ")[1].trim();
+    return x < y ? -1 : x > y ? 1 : 0;
+  })
+  .filter((char) => !char?.dead);
+*/
