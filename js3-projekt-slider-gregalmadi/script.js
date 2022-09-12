10cece5ac609e0954bb8617a53154858a4c18902
Almádi Gergely <109812844+gregalmadi@users.noreply.github.com>
2022-09-12 09:23:35 +0200

    Add files via upload

--- js3-projekt-slider-gregalmadi/script.js
+++ js3-projekt-slider-gregalmadi/script.js
@@ -5,13 +5,33 @@ import gallery from "./images.json" assert { type: "json" };
 const nextImage = document.querySelector(".arrow--right");
 const prevImage = document.querySelector(".arrow--left");
 const mainImage = document.querySelector(".image__main");
-const bubbles = Array.from(document.querySelectorAll(".bubble"));
+let bubbles;
+
 const counter = document.querySelector(".counter");
 const caption = document.querySelector(".caption");
 
+const bubbleContainer = document.querySelector(".bubbles");
+
 const autoSkipper = 5000;
+/*const sliderHeight = "80vh";*/
 
 let imgID = 0;
+
+const imageTemplate = `
+<img class="bubble" src="images/inactive.svg" />
+`;
+
+/*const containerSlider = document.querySelector(".container__slider");
+containerSlider.style.height = sliderHeight;*/
+
+for (let i = 0; i < gallery.length; i++) {
+  bubbleContainer.innerHTML += imageTemplate;
+
+  /*const bubi = document.createElement("div");
+  bubbleContainer.appendChild(bubi);
+  bubi.innerHTML = imageTemplate;*/
+}
+bubbles = Array.from(document.querySelectorAll(".bubble"));
 
 bubbles.forEach((bubble, i) =>
   bubble.addEventListener("click", () => {
@@ -20,12 +40,12 @@ bubbles.forEach((bubble, i) =>
 );
 
 nextImage.addEventListener("click", () => {
-  imgID === 3 ? (imgID = 0) : imgID++;
+  imgID === gallery.length - 1 ? (imgID = 0) : imgID++;
   rollImage(imgID);
 });
 
 prevImage.addEventListener("click", () => {
-  imgID === 0 ? (imgID = 3) : imgID--;
+  imgID === 0 ? (imgID = gallery.length - 1) : imgID--;
   rollImage(imgID);
 });
 
@@ -35,7 +55,7 @@ const rollImage = (imgID) => {
   );
   mainImage.classList.add("fade");
 
-  counter.innerHTML = `${gallery[imgID].id + 1}/4`;
+  counter.innerHTML = `${gallery[imgID].id + 1}/${gallery.length}`;
   caption.innerHTML = `${gallery[imgID].caption}`;
 
   bubbles[imgID].setAttribute("src", "images/active.svg");
@@ -50,6 +70,6 @@ const rollImage = (imgID) => {
 };
 
 setInterval(() => {
-  imgID === 3 ? (imgID = 0) : imgID++;
+  imgID === gallery.length - 1 ? (imgID = 0) : imgID++;
   rollImage(imgID);
 }, autoSkipper);
