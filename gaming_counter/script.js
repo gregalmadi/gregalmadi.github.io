d971d753ef3579606c8513dfc2478bae6aaecab8
Greg Almadi <109812844+gregalmadi@users.noreply.github.com>
2022-09-27 19:05:52 +0200

    Update script.js

--- gaming_counter/script.js
+++ gaming_counter/script.js
@@ -2,7 +2,8 @@
 
 const counter = document.querySelector(".counter");
 
-const startDate = new Date("2022-05-29").getTime();
+//const startDate = new Date("2022-05-29").getTime(); // previous streak: 110 days
+const startDate = new Date("2022-09-27").getTime();
 const now = new Date().getTime();
 
 const diffInDays = Math.trunc((now - startDate) / 1000 / 60 / 60 / 24);
