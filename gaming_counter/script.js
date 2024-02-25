2a537df7b3ddb97ce55f2a8018c7583eee2a431a
Greg Almadi <109812844+gregalmadi@users.noreply.github.com>
2024-02-25 13:28:31 +0100

    Update script.js

--- gaming_counter/script.js
+++ gaming_counter/script.js
@@ -1,19 +1,17 @@
 "use strict";
 
 const counter = document.querySelector(".counter");
-const daysLeft = document.querySelector(".days");
 
-//const startDate = new Date("2022-05-29").getTime(); // previous streak: 110 days
-const timeOfTravel = new Date("2024-12-12").getTime();
-const now = new Date().getTime();
+setInterval(() => {
+  const timeOfTravel = new Date("2024-12-27").getTime();
+  const now = new Date().getTime();
 
-const diffInDays = Math.trunc((timeOfTravel - now) / 1000 / 60 / 60 / 24);
-let diffInSeconds = (timeOfTravel - now) / 1000;
+  const differenceInEpoch = timeOfTravel - now;
 
-const updater = setInterval(()=>{
-  diffInSeconds = (timeOfTravel - now)
-},1000000)
+  const days = Math.trunc(differenceInEpoch / 1000 / 60 / 60 / 24);
+  const hours = Math.trunc((differenceInEpoch / 1000 / 60 / 60) % 24);
+  const minutes = Math.trunc((differenceInEpoch / 1000 / 60) % 60);
+  const seconds = Math.trunc((differenceInEpoch / 1000) % 60);
 
-
-daysLeft.innerHTML = `DAYS LEFT: ${diffInDays}`;
-counter.innerHTML = `SECONDS LEFT: ${diffInSeconds}`;
+  counter.innerHTML = `TIME REMAINING TILL JAPAN 💕: ${days}:${hours}:${minutes}:${seconds}`;
+}, 1000);
