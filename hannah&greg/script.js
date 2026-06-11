ad1df9536895c4d36177d419a0464dee7b167192
Greg Almadi <109812844+gregalmadi@users.noreply.github.com>
2026-06-11 22:17:21 +0800

    Update script.js

--- hannah&greg/script.js
+++ hannah&greg/script.js
@@ -3,6 +3,10 @@
 const counter = document.querySelector(".counter");
 //const counterObon = document.querySelector(".counter_obon");
 
+counter.innerHTML = 'FINALLY TOGETHER 💗🏡👫'
+
+/*
+
 setInterval(() => {
   const timeOfTravel = new Date("2026-03-27").getTime() + new Date().getTimezoneOffset() * 60 * 1000;
   //const timeOfTravelObon = new Date("2024-08-09").getTime() + new Date().getTimezoneOffset() * 60 * 1000;
@@ -21,9 +25,11 @@ setInterval(() => {
   //const minutesObon = Math.trunc((differenceInEpochObon / 1000 / 60) % 60);
   //const secondsObon = Math.trunc((differenceInEpochObon / 1000) % 60);
 
-  counter.innerHTML = `TIME SINCE VISA APPOINTMENT 🪪🏢: ${days}:${String(hours).length < 2 ? "0" + hours : hours}:${String(minutes).length < 2 ? "0" + minutes : minutes}:${
-    String(seconds).length < 2 ? "0" + seconds : seconds
-  }`;
+  
+
+  //counter.innerHTML = `TIME SINCE VISA APPOINTMENT 🪪🏢: ${days}:${String(hours).length < 2 ? "0" + hours : hours}:${String(minutes).length < 2 ? "0" + minutes : minutes}:${
+  //  String(seconds).length < 2 ? "0" + seconds : seconds
+  //}`;
 
   //counter.innerHTML = `TIME REMAINING TILL WEDDING TRIP 💍💕💫: ${days}:${String(hours).length < 2 ? "0" + hours : hours}:${String(minutes).length < 2 ? "0" + minutes : minutes}:${
   //  String(seconds).length < 2 ? "0" + seconds : seconds
@@ -33,10 +39,12 @@ setInterval(() => {
   //  String(seconds).length < 2 ? "0" + seconds : seconds
   //} SECs`;
 
-  /*counterObon.innerHTML = `OBON! 💖: ${daysObon}:${String(hoursObon).length < 2 ? "0" + hoursObon : hoursObon}:${String(minutesObon).length < 2 ? "0" + minutesObon : minutesObon}:${
-    String(secondsObon).length < 2 ? "0" + secondsObon : secondsObon
-  }`;*/
+  //counterObon.innerHTML = `OBON! 💖: ${daysObon}:${String(hoursObon).length < 2 ? "0" + hoursObon : hoursObon}:${String(minutesObon).length < 2 ? "0" + minutesObon : minutesObon}:${
+  //  String(secondsObon).length < 2 ? "0" + secondsObon : secondsObon
+  //}`;
 }, 1000);
+
+*/
 
 
 
